d5b252f818348ad6c5a385a3f93e89473dc25709
Андрей Демьяненко <asiforis@MacBook-Pro-3.local>
2018-09-23 20:25:48 +0300

    Show 10 request with too long send result phase

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ import re
 from argparse import ArgumentParser
 from typing import List, Mapping
 
-requests: Mapping[int, int] = {}
+requests_start_time: Mapping[int, int] = {}
+
+backend_poll_phase_times: Mapping[int, int] = {}
+merge_results_phase_time: Mapping[int, int] = {}
+request_send_result_phase: Mapping[int, int] = {}
+
 requests_durations: List[int] = []
+requests_with_too_long_send_results_phase = []
+MAX_REQUEST_WITH_TOO_LONG_SEND_RESULT_PHASE = 10
 
 main_info_re = re.compile(r'(?P<timestamp>\d+)\s*(?P<id>\d+)\s+(?P<type>\w+)\s(?P<arguments>.*)')
 
 REQUEST_TYPE_START = 'StartRequest'
 REQUEST_TYPE_FINISH = 'FinishRequest'
+REQUEST_TYPE_START_MERGE = 'StartMerge'
+REQUEST_TYPE_START_SEND_RESULT = 'StartSendResult'
 
 
 def percentile(array, percent, key=lambda x: x):
@@ -43,19 +52,42 @@ percentil_95 = functools.partial(percentile, percent=0.95)
 
 def parse_request_record(request_id: int, request_type: str, timestamp: int, arguments: str):
     if request_type == REQUEST_TYPE_START:
-        requests[request_id] = timestamp
+        requests_start_time[request_id] = timestamp
+
+    if request_type == REQUEST_TYPE_START_MERGE:
+        backend_poll_phase_times[request_id] = timestamp
+
+    if request_type == REQUEST_TYPE_START_SEND_RESULT:
+        merge_results_phase_time[request_id] = timestamp
 
     if request_type == REQUEST_TYPE_FINISH:
-        request_start_at = requests[request_id]
+        request_send_result_phase[request_id] = timestamp
+
+        # calculate total time of request
+        request_start_at = requests_start_time[request_id]
         duration = timestamp - request_start_at
 
         requests_durations.append(duration)
+
+        # compare phase duration
+        if len(requests_with_too_long_send_results_phase) < MAX_REQUEST_WITH_TOO_LONG_SEND_RESULT_PHASE:
+            backend_poll_phase_time = backend_poll_phase_times[request_id]
+            merge_phase_time = merge_results_phase_time[request_id]
+            send_results_phase_time = request_send_result_phase[request_id]
+
+            if send_results_phase_time > merge_phase_time and send_results_phase_time > backend_poll_phase_time:
+                requests_with_too_long_send_results_phase.append(request_id)
+
+        # clean already handled results (memory optimization)
+        del requests_start_time[request_id]
+        del backend_poll_phase_times[request_id]
+        del merge_results_phase_time[request_id]
+        del request_send_result_phase[request_id]
 
 
 def parse_line(line: str):
     match = main_info_re.match(line)
     if not match:
-        print("Can't parse string %s", line)
         return
 
     request_id: int = int(match.group('id'))
@@ -76,6 +108,10 @@ def print_result(file_name: str) -> None:
     requests_percentil = int(percentil_95(requests_durations))
     with open(file_name, 'w') as output:
         output.write(f'95-й перцентиль времени работы: {requests_percentil}\n')
+        output.write(f'Идентификаторы запросов с самой долгой фазой отправки результатов пользователю:\n')
+
+        for request_id in requests_with_too_long_send_results_phase:
+            output.write(f'    {request_id}\n')
 
 
 median = functools.partial(percentile, percent=0.5)
