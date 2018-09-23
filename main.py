1de4e07d86b7204c5b8aaa93d3f106517b8b9b94
asiforis <asiforis@gmail.com>
2018-09-23 22:28:44 +0300

    Show in stats total count of not completed request

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import math
 import re
 from argparse import ArgumentParser
 from collections import defaultdict
-from typing import Dict, List
+from typing import DefaultDict, Dict, List
 
 REQUEST_TYPE_START = 'StartRequest'
 REQUEST_TYPE_FINISH = 'FinishRequest'
 REQUEST_TYPE_BACKEND_CONNECT = 'BackendConnect'
+REQUEST_TYPE_BACKEND_OK = 'BackendOk'
 REQUEST_TYPE_BACKEND_ERROR = 'BackendError'
 REQUEST_TYPE_START_MERGE = 'StartMerge'
 REQUEST_TYPE_START_SEND_RESULT = 'StartSendResult'
+
+MAX_REQUEST_WITH_TOO_LONG_SEND_RESULT_PHASE = 10
 
 requests_start_time: Dict[int, int] = {}
 
@@ -20,27 +23,42 @@ request_send_result_phase: Dict[int, int] = {}
 
 requests_durations: List[int] = []
 
-request_connections = defaultdict(dict)
+request_connections: DefaultDict[int, Dict[int, str]] = defaultdict(dict)
+request_connections_response_status: DefaultDict[int, Dict[int, str]] = defaultdict(dict)
 
-backends_replics: Dict[str, int] = {}
-backends_connections = defaultdict(lambda: 0)
-backends_errors = defaultdict(lambda: defaultdict(lambda: 0))
+backends_replicas: Dict[str, int] = {}
+backends_connections: DefaultDict[str, int] = defaultdict(lambda: 0)
+backends_errors: DefaultDict[str, Dict[str, int]] = defaultdict(lambda: defaultdict(lambda: 0))
 
 requests_with_too_long_send_results_phase = []
-MAX_REQUEST_WITH_TOO_LONG_SEND_RESULT_PHASE = 10
 
 general_re = re.compile(r'(?P<timestamp>\d+)\s*(?P<id>\d+)\s+(?P<type>\w+)\s.*')
 backend_connect_re = re.compile(f'.*{REQUEST_TYPE_BACKEND_CONNECT}\s(?P<group>\d+)\s+(?P<url>\S+)')
+backend_ok_re = re.compile(f'.*{REQUEST_TYPE_BACKEND_OK}\s+(?P<group>\d+)')
 backend_error_re = re.compile(f'.*{REQUEST_TYPE_BACKEND_ERROR}\s(?P<group>\d+)\s+(?P<error>.*)')
 
 
-def percentile(array, percent, key=lambda x: x):
+class Counter:
+
+    def __init__(self, default=0):
+        self.value = default
+
+    def __str__(self):
+        return f'{self.value}'
+
+    def increase(self):
+        self.value += 1
+
+
+requests_not_completed = Counter()
+
+
+def percentile(array, percent):
     """
     Find the percentile of a list of values.
 
     @parameter array - is a list of values. Note N MUST BE already sorted.
     @parameter percent - a float value from 0.0 to 1.0.
-    @parameter key - optional key function to compute value from each element of N.
 
     @return - the percentile of the values
     """
@@ -51,10 +69,10 @@ def percentile(array, percent, key=lambda x: x):
     f = math.floor(k)
     c = math.ceil(k)
     if f == c:
-        return key(array[int(k)])
+        return array[int(k)]
 
-    d0 = key(array[int(f)]) * (c - k)
-    d1 = key(array[int(c)]) * (k - f)
+    d0 = array[int(f)] * (c - k)
+    d1 = array[int(c)] * (k - f)
 
     return d0 + d1
 
@@ -90,7 +108,8 @@ def parse_request_record(request_id: int, request_type: str, timestamp: int):
             if send_results_phase_time > merge_phase_time and send_results_phase_time > backend_poll_phase_time:
                 requests_with_too_long_send_results_phase.append(request_id)
 
-
+        if not all(request_connections_response_status[request_id].values()):
+            requests_not_completed.increase()
 
         # clean already handled results (memory optimization)
         del requests_start_time[request_id]
@@ -99,12 +118,19 @@ def parse_request_record(request_id: int, request_type: str, timestamp: int):
         del request_send_result_phase[request_id]
 
         del request_connections[request_id]
+        del request_connections_response_status[request_id]
 
 
-def parse_request_backend_connect_request(request_id: int, group_id: int, backend_url: str):
+def parse_request_backend_connect_record(request_id: int, group_id: int, backend_url: str):
     request_connections[request_id][group_id] = backend_url
-    backends_replics[backend_url] = group_id
+    request_connections_response_status[request_id][group_id] = False
+
+    backends_replicas[backend_url] = group_id
     backends_connections[backend_url] += 1
+
+
+def parse_request_backend_ok_record(request_id: int, group_id: int):
+    request_connections_response_status[request_id][group_id] = True
 
 
 def parse_request_backend_error_record(request_id: int, group_id: int, error: str):
@@ -131,7 +157,7 @@ def parse_line(line: str):
         group_id = int(match.group('group'))
         backend_url = match.group('url')
 
-        parse_request_backend_connect_request(request_id=request_id, group_id=group_id, backend_url=backend_url)
+        parse_request_backend_connect_record(request_id=request_id, group_id=group_id, backend_url=backend_url)
 
     elif request_type == REQUEST_TYPE_BACKEND_ERROR:
         match = backend_error_re.match(line)
@@ -143,6 +169,17 @@ def parse_line(line: str):
         group_id = int(match.group('group'))
 
         parse_request_backend_error_record(request_id=request_id, group_id=group_id, error=error)
+
+    elif request_type == REQUEST_TYPE_BACKEND_OK:
+
+        match = backend_ok_re.match(line)
+        if not match:
+            print(f"Can't parse backend arguments from line: {line}")
+            return
+
+        group_id = int(match.group('group'))
+
+        parse_request_backend_ok_record(request_id=request_id, group_id=group_id)
     else:
         parse_request_record(request_id=request_id, request_type=request_type, timestamp=timestamp)
 
@@ -157,7 +194,7 @@ def print_result(file_name: str) -> None:
     requests_time_percentil = int(percentil_95(requests_durations))
 
     replica_groups = defaultdict(list)
-    for backend, group_id in backends_replics.items():
+    for backend, group_id in backends_replicas.items():
         replica_groups[group_id].append(backend)
 
     with open(file_name, 'w') as output:
@@ -167,10 +204,13 @@ def print_result(file_name: str) -> None:
         for request_id in requests_with_too_long_send_results_phase:
             output.write(f'    {request_id}\n')
 
+        output.write(f'Запросов с неполным набором ответивших ГР: {requests_not_completed}\n')
         output.write('Обращения и ошибки по бэкандам\n')
-        for group, backends in replica_groups.items():
+        groups = sorted(replica_groups.keys())
+        for group in groups:
             output.write(f'    ГР {group}\n')
 
+            backends = sorted(replica_groups[group])
             for backend in backends:
                 connections = backends_connections[backend]
 
@@ -183,8 +223,6 @@ def print_result(file_name: str) -> None:
                 for error, count in errors.items():
                     output.write(f'                {error}: {count}\n')
 
-
-median = functools.partial(percentile, percent=0.5)
 
 if __name__ == '__main__':
     argparser = ArgumentParser()
